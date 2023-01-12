c660fdf96e690a4167f9c1bc3c00d7b0fa073bc4
Dmitriy <dmitriy.folomkin@gmail.com>
2023-01-12 15:09:43 +0300

    start learn kotlin-sb

---
+++ src/test/kotlin/com/example/sb_kotlin/SbKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.sb_kotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SbKotlinApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
